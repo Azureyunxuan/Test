5b90b374ecc379a1781ad6203587a15905910adc
Azureyunxuan <961284641@qq.com>
2016-03-02 10:26:03 +0800

    no message

---
+++ TestForMy/TestForMy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForMy
+//
+//  Created by Azure on 16/3/1.
+//  Copyright © 2016年 单云轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestForMy/TestForMy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestForMy
+//
+//  Created by Azure on 16/3/1.
+//  Copyright © 2016年 单云轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
